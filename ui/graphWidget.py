fd36966ffb86da8b14d9cac3cedced56354486b6
Andrew Reilley <reilleya@mit.edu>
2019-12-20 23:25:49 -0500

    Added color coding to graph labels

--- ui/graphWidget.py
+++ ui/graphWidget.py
@@ -5,6 +5,9 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from PyQt5.QtWidgets import QApplication
 
+FORCE_COLOR = 'tab:blue'
+PRESSURE_COLOR = 'tab:red'
+
 class GraphWidget(FigureCanvas):
     def __init__(self, parent):
         super(GraphWidget, self).__init__(Figure())
@@ -31,17 +34,19 @@ class GraphWidget(FigureCanvas):
         self.forceAxes.set_xlabel('Time (s)')
         if force is not None:
             self.forceAxes.set_ylabel('Force ({})'.format(app.getUserUnit('N')))
+            self.forceAxes.yaxis.label.set_color(FORCE_COLOR)
         if pressure is not None:
             self.pressureAxes.set_ylabel('Pressure ({})'.format(app.getUserUnit('Pa')))
+            self.pressureAxes.yaxis.label.set_color(PRESSURE_COLOR)
         self.draw()
 
     def plotData(self, time, force=None, pressure=None):
         self.forceAxes.clear()
         self.pressureAxes.clear()
         if force is not None:
-            self.forceAxes.plot(time, force, color='tab:blue')
+            self.forceAxes.plot(time, force, color=FORCE_COLOR)
         if pressure is not None:
-            self.pressureAxes.plot(time, pressure, color='tab:red')
+            self.pressureAxes.plot(time, pressure, color=PRESSURE_COLOR)
         self.forceAxes.set_ylim(bottom=0)
         self.pressureAxes.set_ylim(bottom=0)
         self.draw()
